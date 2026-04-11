4ec4840a2ebca0f42be92f3d263bccb1cc55df56
simon sekri <simonsekri@gmail.com>
2026-04-11 17:46:09 +0200

    ADD : Student : code complet

---
+++ src/Student.java
@@ -0,0 +1,46 @@
+public class Student {
+    private String firstname;
+    private String lastname;
+    private SchoolYear schoolYear;
+
+    public Student(String firstname, String lastname, SchoolYear schoolYear) {
+        setFirstname(firstname);
+        setLastname(lastname);
+
+        if (schoolYear == null) {
+            throw new IllegalArgumentException("L'année scolaire ne peut pas être null");
+        }
+        this.schoolYear = schoolYear;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public SchoolYear getSchoolYear() {
+        return schoolYear;
+    }
+
+    public void setFirstname(String firstname) {
+        if ( firstname == null || firstname.isBlank()) {
+            throw new IllegalArgumentException("Le prénom ne peux pas être null");
+        }
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        if ( lastname == null || lastname.isBlank()) {
+            throw new IllegalArgumentException("Le nom de famille ne peux pas être null");
+        }
+        this.lastname = lastname;
+    }
+
+    public String toString() {
+        return firstname + " " + lastname + " est étudiant(e) en " + schoolYear;
+    }
+
+}
